c80fb6aadc3ef41c549bb41ada74542d7224916e
longy0115 <longy0115@163.com>
2018-05-25 17:26:27 +0800

    Add following

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 use App\Notifications\ResetPassword;
-
+use Auth;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -49,12 +49,46 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids, Auth::user()->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
     public function statuses()
     {
         return $this->hasMany(Status::class);
     } 
+    /*获取粉丝关系列表*/
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+    /*获取用户关注人列表*/
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+    /*关注*/
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+    /*取消关注*/
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+    /*判断当前登录的用户 A 是否关注了用户 B*/
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 
 }
